824883700e294abce2f1561ca477b22569e38c60
Devanand Rai <devanandrai283@gmail.com>
2026-02-09 14:41:17 +0530

    Fix local+prod DB config and Docker setup

--- src/test/java/com/vendor/vendorregistration/VendorRegistrationApplicationTests.java
+++ src/test/java/com/vendor/vendorregistration/VendorRegistrationApplicationTests.java
@@ -2,12 +2,14 @@ package com.vendor.vendorregistration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class VendorRegistrationApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
+
